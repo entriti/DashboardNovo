4ca4f48c7bfc4965ed749cfd4fe869c9ac565408
entriti <ti02@entri.com.br>
2024-10-22 09:52:21 -0300

    Update login.js

--- recursos/login.js
+++ recursos/login.js
@@ -1,11 +1,16 @@
 function verifyPin() {
     const pin = document.getElementById('pinInput').value;
     const user = document.getElementById('user').value;
-    const correctPIN = '139127';
-    const correctUser = 'admin';
 
-    if (pin === correctPIN && user === correctUser) {
-        window.location.href = "https://entriti.github.io/DashboardNovo/dashboard.html#";
+    const credentials = [
+        { user: 'admin', pin: 'madira', url: 'https://entriti.github.io/DashboardNovo/dashboard.html#' },
+        { user: 'operacional', pin: 'ope789', url: 'https://entriti.github.io/DashboardNovo/operacional.html#' },
+    ];
+
+    const foundUser = credentials.find(cred => cred.user === user && cred.pin === pin);
+
+    if (foundUser) {
+        window.location.href = foundUser.url;
     } else {
         document.getElementById('error-message').style.display = 'block';
     }
